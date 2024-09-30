0b97810215bcf2da6bcc5fe1a7dcf691a329dd84
pa-afk <alina.semjonova07@gmail.com>
2024-09-30 13:20:29 +0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,13 @@
+a = 10 # a - int type variable
+b = 5.0
+
+print(a + b )
+print( a - b )
+print( a / b )
+print( a * b )
+print( a ** b )
+print( a // b )
+
+user =input('enter your name: ')
+print(f"Hello {user}!")
+user2 = int(input('Enter number:'))
