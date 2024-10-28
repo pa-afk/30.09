ecfa35dacdd271d4a39847586a682d6b8172ca09
arseny13-52 <arsenyatorin@gmail.com>
2024-10-28 12:40:49 +0200

    Create uzd1.py

---
+++ uzd1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 12:08:21 2024
+
+@author: 6vsk.viesis1
+# %%
+"""
+
+
+x=1
+result= 5*x**3-x**2+7*x-6
+x=int(input("Enter x:"))
+print(result)
+
+
+
+
+x=4
+y=2
+results= (2+x)/(x*y)
+x=int(input("Enter x:"))
+y=int(input("Enter y:"))
+print(result)
+
+x=3
+y=4
+result=(2**(x*y))/(x*y)
+x=int(input("Enter x:"))
+y=int(input("Enter y:"))
+print(result)
+
+x=5
+y=3
+(x*y)**0.5
+x=int(input("Enter x:"))
+y=int(input("Enter y:"))
+print(result)
+
+
+
+
+
+
+
+
+
