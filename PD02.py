946ad7c176d5c1dafc61279fdc43682520237669
pa-afk <alina.semjonova07@gmail.com>
2024-12-16 13:16:46 +0200

    Add files via upload

---
+++ PD02.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 13:15:14 2024
+
+@author: 6vsk.viesis1
+"""
+
+#import random
+
+# 2. uzdevums
+#sum = 0
+#afor i in range(a):
+ #sum = sum + a
+# print(sum)
+ 
+#sum = 0
+#a = int(input("Ēnter:"))
+#for i in range(1,a):
+#  sum = sum + i
+#  print(sum)
+# %%
+ #  3. uzdevums 
+sum = 1
+a = int(input("Enter skaitli:"))
+for i in range(1,a):
+    sum = sum * i
+    print(sum)
+ 
+# %%
+ 
+# 4. uzdevums
+a =int(input("Enter skaitli:"))
+for i in range(a, -1, -1):
+     print(i)
+     
+# %%
+     
+# 5. uzdevums
+#augstums = 19
+#x = 19
+
+#for i in range(0, augstums):
+ #   print(' '*(augstums + i), end="")
+ #   print('*'*x,end='')
+  #  print()
+  #  x = x - 2
+# %%
+# 5.1 uzdevums
+augstums = int(input("Enter skaitli:"))
+x = int(input("Enter skaitli:"))
+
+for i in range(0, augstums):
+    print(' '*(augstums + i), end="")
+    print('*'*x,end='')
+    print()
+    x = x - 2
+ 
+# %%
+
+# 6. uzdevums
+
+#for i in range(10):
+# x = int(input("Enter skaitlis:"))
+ #y = random.choice([1,2,3,4,5,6,7,8,9,10])
+# print(random.choice([1,2,3,4,5,6,7,8,9,10]))
+ #if x == y:
+  #   print("Uzminets")
+ #elif x > y:
+ #   print("Mazaks")
+# elif x < y:
+ #   print("Lielaks")
+    
