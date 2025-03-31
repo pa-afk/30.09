d0226d23b0c2298a30bd580d2ae217316b537c94
pa-afk <alina.semjonova07@gmail.com>
2025-03-31 13:14:46 +0300

    Add files via upload

---
+++ Skoleni.py
@@ -0,0 +1,25 @@
+class Skoleni:
+    counter = 0
+    def __init__(self, vards, uzvards, vecums,klase):
+        self.vards = vards
+        self.uzvards = uzvards
+        self.vecums = vecums
+        self.klase = klase
+        self.id = Skoleni.counter
+        Skoleni.counter += 1
+    def __str__(self):
+        return f'{self.id} {self.vards} {self.uzvards} {self.klase}'
+    def pievienot_atzimi(self, atzime):
+        if atzime = (0,11):
+            
+
+    def iegut_videjo_atzimi(self):
+        counter = 0
+        for i in atzime:
+            counter += 1 
+        return counter/len(atzime)
+
+Skolens1 = 
+        
+    
+
